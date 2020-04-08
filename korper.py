dda7e4218df048e225386a088587d3188bbebb55
JorgePerC <42452493+JorgePerC@users.noreply.github.com>
2020-04-08 01:06:53 -0500

    Funciona :)
    Pequeñas modificaciones

--- korper.py
+++ korper.py
@@ -7,6 +7,7 @@ import pprint
 import time
 from gspread.exceptions import APIError
 from oauth2client.transport import request
+import random
 
 scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
 creds = ServiceAccountCredentials.from_json_keyfile_name('Credentials/requests_SJ_Credentials.json', scope)
@@ -18,14 +19,21 @@ listaErrores1 = []
 listaErrores2 = []
 def iniciarApp(diccionario):
     random.shuffle(diccionario)
-    filaAleman = diccionario.pop(1)
-    palabraAleman = filaAleman.get("Wort")
-    palabraEspñaol = filaAleman.get("Übersetzung")
-    palabraGenero = filaAleman.get("Artikel")
-    palabraPlural = filaAleman.get("Pural")
-    
-    print(palabraAleman) 
-    while not(len(diccionario)) == 0:
+    # Lo que pasa aquí, que se repita la misma palabra es porque
+    # filaAleman = diccionario.pop(1)
+    # palabraAleman = filaAleman.get("Wort")...
+    # Está fuera del While, entonces, eso no se repite.
+    # Lo voy a meter.
+
+    while not(len(diccionario) == 0): #Aquí corregí un paréntesis. Antes "not(len(diccionario)) == 0:"
+        filaAleman = diccionario.pop(1)
+        palabraAleman = filaAleman.get("Wort")
+        palabraEspñaol = filaAleman.get("Übersetzung")
+        palabraGenero = filaAleman.get("Artikel")
+        palabraPlural = filaAleman.get("Pural")
+        
+        print(palabraAleman)
+        #Me dí todo lo de arriba
         respuesta1 = input("género ")
         if respuesta1 in palabraGenero:
             respuesta2 = input("plural ")
@@ -49,22 +57,23 @@ def iniciarApp(diccionario):
         if (input("Seguir? ") == "no"): 
             break
         else:
-         pass
+            pass
 
+
+#-----Main-----
 iniciarApp(words)
 respuestaTry = input("Willst du noch einaml die Fehler wiederholen ")
 if respuestaTry == "si":
     iniciarApp(listaErrores2)
 else:
     print(listaErrores1)
-
-
-
-
     
-   
+#Funcionó bien una vez, después:
+# TypeError: argument of type 'NoneType' is not iterable
+# Fue porque modifiqué el docs, jajaja
 
-
+#Cuando revisas los errores, y sólo tienes uno, al hacerle el pop(1)
+#Te manda error
 
 # column = 10
 # result = sheet.row_values(10)
